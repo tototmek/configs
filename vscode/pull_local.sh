c139c71111429d6b5062c7a0607128ed66190914
Tomek Żebrowski <85454935+tototmek@users.noreply.github.com>
2023-07-31 15:12:56 +0200

    Add files via upload

---
+++ vscode/pull_local.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Copying settings from ~/.config/Code/User/"
+cp ~/.config/Code/User/settings.json settings.json
+cp ~/.config/Code/User/keybindings.json keybindings.json
+
---
+++ vscode/push_local.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Copying settings from ~/.config/Code/User"
+cp settings.json ~/.config/Code/User/settings.json
+cp keybindings.json ~/.config/Code/User/keybindings.json 
+
